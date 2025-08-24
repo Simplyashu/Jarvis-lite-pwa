f2191d2d4c9d8d0bb3ef0eb7c4d06aa0fea3bc1c
Simplyashu <109404045+Simplyashu@users.noreply.github.com>
2025-08-24 17:03:00 +0530

    Add all index barrel files and README for initial project structure

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+// Barrel file for components
+export { default as Dashboard } from './Dashboard';
+export { default as Notes } from './Notes';
+export { default as Todo } from './Todo';
+export { default as Reminders } from './Reminders';
+export { default as MathSolver } from './MathSolver';
+export { default as Flashcards } from './Flashcards';
+export { default as Pomodoro } from './Pomodoro';
+export { default as Settings } from './Settings';
+export { default as Help } from './Help';
